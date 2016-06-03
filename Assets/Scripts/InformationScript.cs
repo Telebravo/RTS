531b646eeef7b2564c3a01a5baf837bec6d6b881
Voldakk <voldakk@outlook.com>
2016-06-03 18:04:55 +0200

    General fixes for the UI

--- Assets/Scripts/InformationScript.cs
+++ Assets/Scripts/InformationScript.cs
@@ -9,17 +9,35 @@ public class InformationScript : MonoBehaviour {
     public Text generalInfo;
     public Text timer;
     public float tid2;
-    public int tid; 
-	void Start () {
-	}
+    public int tid;
 	
-	
-	void Update () {
-        Unit unit = CameraControlls.selectedObjects[0].GetComponent<Unit>(); 
-        hitPoints.text = "HP: " + unit.cHealth.currentHealth.ToString();//unit Health
-        unitName.text = unit.displayName; //unit navn
-        generalInfo.text = "Weapon: " + unit.weapon.ToString() + "\n" + "Optics: " + unit.optics.ToString() + "\n" + "Armor: " + unit.armor.ToString() + "\n" + "Movement Speed: " + unit.movementSpeed.ToString() + "\n" + "Range: " + unit.weapon.range.ToString() + "\n" + "Rate of fire: " + unit.weapon.firerate.ToString() + "\n" + "Damage: " + unit.weapon.damage.ToString() + " " + unit.weapon.damageType.ToString();
-        //Lang string med generel info
+	void Update ()
+    {
         timer.text = tid.ToString();
+        if (CameraControlls.selectedObjects.Count > 0)
+        {
+            Unit unit = CameraControlls.selectedObjects[0].GetComponent<Unit>();
+            if (unit != null)
+            {
+                hitPoints.text = "HP: " + unit.cHealth.currentHealth.ToString();//unit Health
+                unitName.text = unit.displayName; //unit navn
+                generalInfo.text = "Weapon: " + unit.weapon.ToString() + "\n" + "Optics: " + unit.optics.ToString() + "\n" + "Armor: " + unit.armor.ToString() + "\n" + "Movement Speed: " + unit.movementSpeed.ToString() + "\n" + "Range: " + unit.weapon.range.ToString() + "\n" + "Rate of fire: " + unit.weapon.firerate.ToString() + "\n" + "Damage: " + unit.weapon.damage.ToString() + " " + unit.weapon.damageType.ToString();
+                //Lang string med generel info
+            }
+            else
+            {
+                Clear();
+            }
+        }
+        else
+        {
+            Clear();
+        }
+    }
+    void Clear()
+    {
+        hitPoints.text = "";
+        unitName.text = "";
+        generalInfo.text = "";
     }
 }
