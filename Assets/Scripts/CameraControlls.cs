3be33006632b2853cfaaa9a84ff5b7c095481922
Voldakk <voldakk@outlook.com>
2016-06-04 15:23:04 +0200

    Added support for different cursors
    Added some different cursors as a way to signalise the current actions
    performed by the mouse buttons

--- Assets/Scripts/CameraControlls.cs
+++ Assets/Scripts/CameraControlls.cs
@@ -4,11 +4,18 @@ using System.Collections;
 using System.Collections.Generic;
 public class CameraControlls : MonoBehaviour
 {
+    public Texture2D[] cursorTextures;
+
     //Liste over synlige objekter
     public static List<Transform> visibleObjects;
     public List<Transform> _visible;
     //Maks antall valgte units
     public int selectionLimit;
+    
+    //objektet musen for øyeblikket holder over, men ikke nødvendigvis har valgt
+    public static Transform holdOverObject;
+    public static Vector3 mouseWorldPositionUnitLayer;
+    public static Vector3 mouseWorldPosition;
 
     //Objektet som er valgt
     public static List<Transform> selectedObjects;
@@ -38,55 +45,56 @@ public class CameraControlls : MonoBehaviour
         layermask = 1 << layer;
         visibleObjects = new List<Transform>();
         selectedObjects = new List<Transform>();
+        GameManager.cameraControlls = this;
     }
     void Start ()
     {
         //Henter kamera koponenten
         camera = GetComponent<Camera>();
-	}
-    public static Rect GUIRectWithObject(GameObject go)
-    {
-        Vector3 cen = go.GetComponent<Renderer>().bounds.center;
-        Vector3 ext = go.GetComponent<Renderer>().bounds.extents;
-        Vector2[] extentPoints = new Vector2[8]
-         {
-               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
-         };
-        Vector2 min = extentPoints[0];
-        Vector2 max = extentPoints[0];
-        foreach (Vector2 v in extentPoints)
-        {
-            min = Vector2.Min(min, v);
-            max = Vector2.Max(max, v);
-        }
-        return new Rect(min.x, min.y, max.x - min.x, max.y - min.y);
+        Cursor.lockState = CursorLockMode.Confined;
+        SetCursor(Cursors.Arrow);
     }
-    public static Vector2 WorldToGUIPoint(Vector3 world)
-    {
-        Vector2 screenPoint = Camera.main.WorldToScreenPoint(world);
-        //screenPoint.y = (float)Screen.height - screenPoint.y;
-        return screenPoint;
-    }
-
     void Update ()
     {
-        if (Input.GetKey(KeyCode.LeftControl))
-            return;
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+           // Cursor.SetCursor
+        }
 
+        //Finner objektet man paker på
+        holdOverObject = null;
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 10000, layermask, QueryTriggerInteraction.Ignore))
+        {
+            mouseWorldPositionUnitLayer = hit.point;
+            holdOverObject = hit.transform;
+            cSelectable = holdOverObject.GetComponent<CSelectable>();
+            if (cSelectable != null) ;
+
+        }
+        //Musepekeren sin posison i verden
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 10000, 1, QueryTriggerInteraction.Ignore))
+        {
+            mouseWorldPosition = hit.point;
+        }
+
+        //Kan ikke velge og flytte ting om man holder inne control
+        if (Input.GetKey(KeyCode.LeftControl))
+        return;
+
+        //
         _visible = visibleObjects;
+
+        //Når man trykker ned venste museknapp
        if (Input.GetMouseButtonDown(0))
        {
             //Posisjonen når museknapen ble trykket ned
             mp1 = Input.mousePosition;
             selectionAreaPanel.gameObject.SetActive(true);
         }
+       //Så lenge venstre musenkapp er nede
        if(Input.GetMouseButton(0))
        {
             //Den nåværende posisjonen
@@ -97,12 +105,13 @@ public class CameraControlls : MonoBehaviour
             selectionAreaPanel.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, mp1.x <= mp2.x ? mp2.x - mp1.x : mp1.x - mp2.x);
             selectionAreaPanel.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,   mp1.y <= mp2.y ? mp2.y - mp1.y : mp1.y - mp2.y);
         }
+        //Når man slipper venstre museknapp
         if (Input.GetMouseButtonUp(0))
         {
             //Gjemmer grafikken
             selectionAreaPanel.gameObject.SetActive(false);
 
-            //
+            //Husker de forige valgte objektene
             previouslySelectedObjects = new List<Transform>(selectedObjects);
             selectedObjects.Clear();
 
@@ -188,63 +197,6 @@ public class CameraControlls : MonoBehaviour
             }
         }
     }
-    /*void Select ()
-    {
-        Debug.Log("Camera: Selecting");
-        ray = camera.ScreenPointToRay(Input.mousePosition);
-
-        //Ser om vi treffer noe
-        if (Physics.Raycast(ray, out hit, 100))
-        {
-            //Husker det potensielt forrige objektet som var valgt
-            lastSelected = selected;
-            selected = hit.transform;
-
-            //Om vi har valgt en ny unit
-            if (selected == lastSelected)
-            {
-                return;
-            }
-            else if (cSelectable != null)
-            {
-                //Melder fra tld en forrige at den ikke er valgt lenger
-                cSelectable.SendMessage("Deselected");
-            }
-
-            //Henter komponenten til det nye objektet
-            cSelectable = selected.GetComponent<CSelectable>();
-
-            //Om det er noe som kan velges
-            if (cSelectable != null)
-            {
-                //Melder fra til objektet at det er valgt
-                cSelectable.SendMessage("Selected");
-                cMoveable = selected.GetComponent<CMoveable>();
-            }
-            //Om det ikke kan bli valgt
-            else
-            {
-                //Resetter
-                selected = null;
-                lastSelected = null;
-                cSelectable = null;
-                cMoveable = null;
-            }
-        }
-        //Om vi ikke trykket på noe
-        else
-        {
-            //Melder fra til det potensielt forrige valgte objektet at det ikke er valgt lenger
-            if (cSelectable != null)
-                cSelectable.SendMessage("Deselected");
-
-            //Resetter
-            selected = null;
-            lastSelected = null;
-            cSelectable = null;
-            cMoveable = null;
-        }
-    }*/
     void Move ()
     {
         //Finner posisjon i verden utifra musepeker
@@ -256,18 +208,7 @@ public class CameraControlls : MonoBehaviour
         }
     }
 
-    /*void DrawSelectionRect()
-    {
-        Rect r = GUIRectWithObject(selected.gameObject);
-        Debug.Log(string.Format("X:{0} - {1} Y: {2} - {3}", r.xMin, r.xMax, r.yMin, r.yMax));
-        panel.position = new Vector3(r.xMin - 5, r.yMin - 5);
-        panel.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, r.width + 10);
-        panel.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, r.height + 10);
-
-        Debug.Log(panel.position.y);
-    }*/
-
-    public static void Delect(Transform transfom)
+    public static void Select(Transform transfom)
     {
         if (!selectedObjects.Contains(transfom))
         {
@@ -283,4 +224,56 @@ public class CameraControlls : MonoBehaviour
             transfom.GetComponent<CSelectable>().SendMessage("Deselected");
         }
     }
+
+    public void SetCursor(Cursors cursor)
+    {
+        switch(cursor)
+        {
+            case Cursors.Arrow:
+                Cursor.SetCursor(cursorTextures[(int)cursor], new Vector2(0, 0), CursorMode.Auto);
+                break;
+            case Cursors.Select:
+                Cursor.SetCursor(cursorTextures[(int)cursor], new Vector2(0, 0), CursorMode.Auto);
+                break;
+            case Cursors.Attack:
+                Cursor.SetCursor(cursorTextures[(int)cursor], new Vector2(0, 0), CursorMode.Auto);
+                break;
+            case Cursors.Load:
+                Cursor.SetCursor(cursorTextures[(int)cursor], new Vector2(16, 0), CursorMode.Auto);
+                break;
+            case Cursors.Unload:
+                Cursor.SetCursor(cursorTextures[(int)cursor], new Vector2(16, 0), CursorMode.Auto);
+                break;
+        }
+    }
+    public static Rect GUIRectWithObject(GameObject go)
+    {
+        Vector3 cen = go.GetComponent<Renderer>().bounds.center;
+        Vector3 ext = go.GetComponent<Renderer>().bounds.extents;
+        Vector2[] extentPoints = new Vector2[8]
+         {
+               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
+         };
+        Vector2 min = extentPoints[0];
+        Vector2 max = extentPoints[0];
+        foreach (Vector2 v in extentPoints)
+        {
+            min = Vector2.Min(min, v);
+            max = Vector2.Max(max, v);
+        }
+        return new Rect(min.x, min.y, max.x - min.x, max.y - min.y);
+    }
+    public static Vector2 WorldToGUIPoint(Vector3 world)
+    {
+        Vector2 screenPoint = Camera.main.WorldToScreenPoint(world);
+        return screenPoint;
+    }
 }
+public enum Cursors { Arrow, Select, Attack, Load, Unload}
